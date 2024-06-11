fa18d6152d565d5ed0f4f62c4e800382885c5bc6
arunp <arun.puri198126@gmail.com>
2024-06-11 19:24:47 +0530

    end of lesson five

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Specification;
 
 namespace Core.Interfaces
 {
     public interface IProductRepository
     {
-        Task<Product>GetProductByIdAsync(int Id);
-        Task<IReadOnlyList<Product>>GetProductsAsync();
-        Task<IReadOnlyList<ProductBrand>>GetProductBrandsAsync();
-        Task<IReadOnlyList<ProductType>>GetProductTypesAsync();
+        Task<Product> GetProductByIdAsync(int Id);
+        Task<IReadOnlyList<Product>> GetProductsAsync(ProductSpecPrams productSpecPrams);
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+        Task<int> CountAsync(ProductSpecPrams productSpecPrams);
+        
     }
 }
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,7 +1,10 @@
 using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specification;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace Infrastructure.Data
 {
@@ -12,6 +15,8 @@ namespace Infrastructure.Data
         {
             _storeContext = storeContext;
         }
+
+
 
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
         {
@@ -26,17 +31,73 @@ namespace Infrastructure.Data
             .FirstOrDefaultAsync(p => p.Id == Id);
         }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(ProductSpecPrams productSpecPrams)
         {
-            return await _storeContext.Products
-            .Include(p => p.ProductBrand)
-            .Include(p => p.ProductType)
-            .ToListAsync();
+            IQueryable<Product> query = _storeContext.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType);
+
+            /*---Filter----*/
+            if (productSpecPrams.BrandId.HasValue)
+            {
+                query = query.Where(p => p.ProductBrandId == productSpecPrams.BrandId.Value);
+            }
+            if (productSpecPrams.TypeId.HasValue)
+            {
+                query = query.Where(p => p.ProductTypeId == productSpecPrams.TypeId.Value);
+            }
+            /*-----*/
+
+            //Sourting
+            query = productSpecPrams.Sort switch 
+            {
+                "priceAsc" => query.OrderBy(x => x.Price),
+                "priceDesc" => query.OrderByDescending(x => x.Price),
+                _ => query.OrderBy(x => x.Name)
+            };
+
+            //Searching
+            if (!string.IsNullOrEmpty(productSpecPrams.Search))
+            {
+                query = query.Where(p => p.Name.ToLower().Contains(productSpecPrams.Search));
+            }
+
+            //Pagination
+            query = query.Skip(productSpecPrams.PageSize * (productSpecPrams.PageIndex - 1)).Take(productSpecPrams.PageSize);
+
+            return await query.ToListAsync();
+            
+            // _storeContext.Products
+            // .Include(p => p.ProductBrand)
+            // .Include(p => p.ProductType)
+            // .OrderBy(x=>x.Name)
+            // .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
         {
             return await _storeContext.ProductTypes.ToListAsync();
+        }
+
+        public async Task<int> CountAsync(ProductSpecPrams productSpecPrams)
+        {
+            IQueryable<Product> query = _storeContext.Products;
+            if (productSpecPrams.BrandId.HasValue)
+            {
+                query = query.Where(p => p.ProductBrandId == productSpecPrams.BrandId.Value);
+            }
+            if (productSpecPrams.TypeId.HasValue)
+            {
+                query = query.Where(p => p.ProductTypeId == productSpecPrams.TypeId.Value);
+
+            }
+
+            if (!string.IsNullOrEmpty(productSpecPrams.Search))
+            {
+                query = query.Where(p => p.Name.ToLower().Contains(productSpecPrams.Search));
+            }
+
+            return await query.CountAsync();
         }
     }
 }
